e107fbe18b2a5660a1270615138a3bb2daabc49a
Weslley Braga <wesbragagt@gmail.com>
2019-03-04 19:41:59 -0600

    pushing data to firebase

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// 
+//
 
 // FIREBASE CONFIG
 var config = {
-    apiKey: "AIzaSyDQ5qTtJ_H8RNDcFST-d7W20xqUnUKAlPE",
-    authDomain: "employeedatamanagement-68d33.firebaseapp.com",
-    databaseURL: "https://employeedatamanagement-68d33.firebaseio.com",
-    projectId: "employeedatamanagement-68d33",
-    storageBucket: "employeedatamanagement-68d33.appspot.com",
-    messagingSenderId: "887296605908"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyDQ5qTtJ_H8RNDcFST-d7W20xqUnUKAlPE",
+  authDomain: "employeedatamanagement-68d33.firebaseapp.com",
+  databaseURL: "https://employeedatamanagement-68d33.firebaseio.com",
+  projectId: "employeedatamanagement-68d33",
+  storageBucket: "employeedatamanagement-68d33.appspot.com",
+  messagingSenderId: "887296605908"
+};
+firebase.initializeApp(config);
 
 var database = firebase.database();
 
@@ -18,15 +18,31 @@ var role = "";
 var startDate = "";
 var monthlyRate = 0;
 
-$("#employee-submit-btn").on("click", function(event){
-    event.preventDefault();
+$("#employee-submit-btn").on("click", function(event) {
+  event.preventDefault();
 
-    // Object
+  // Object
 
-    employee = $("#employee-name-input").val().trim();
-    role = $("#employee-role-input").val().trim();
-    startDate = $("#employee-start-date").val().trim();
-    monthlyRate = $("#employee-monthly-rate").val().trim();
+  employee = $("#employee-name-input")
+    .val()
+    .trim();
+  role = $("#employee-role-input")
+    .val()
+    .trim();
+  startDate = $("#employee-start-date")
+    .val()
+    .trim();
+  monthlyRate = $("#employee-monthly-rate")
+    .val()
+    .trim();
 
+  console.log(employee, role, startDate, monthlyRate);
 
-})
+  database.ref().push({
+    employee: employee,
+    role: role,
+    startDate: startDate,
+    monthlyRate: monthlyRate,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
+  });
+});
