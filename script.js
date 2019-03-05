a3ff8a7071db80ea4beb7acc744efb1b93687f20
Weslley Braga <wesbragagt@gmail.com>
2019-03-04 19:32:27 -0600

    firebase basics

---
+++ script.js
@@ -0,0 +1,32 @@
+// 
+
+// FIREBASE CONFIG
+var config = {
+    apiKey: "AIzaSyDQ5qTtJ_H8RNDcFST-d7W20xqUnUKAlPE",
+    authDomain: "employeedatamanagement-68d33.firebaseapp.com",
+    databaseURL: "https://employeedatamanagement-68d33.firebaseio.com",
+    projectId: "employeedatamanagement-68d33",
+    storageBucket: "employeedatamanagement-68d33.appspot.com",
+    messagingSenderId: "887296605908"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var employee = "";
+var role = "";
+var startDate = "";
+var monthlyRate = 0;
+
+$("#employee-submit-btn").on("click", function(event){
+    event.preventDefault();
+
+    // Object
+
+    employee = $("#employee-name-input").val().trim();
+    role = $("#employee-role-input").val().trim();
+    startDate = $("#employee-start-date").val().trim();
+    monthlyRate = $("#employee-monthly-rate").val().trim();
+
+
+})
